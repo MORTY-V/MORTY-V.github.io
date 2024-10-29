74e361c3e3b80c8b408d503d671d4a2e1b09ef03
MORTY-V <mortonfv@gmail.com>
2024-10-29 10:09:29 -0600

    js ica added

---
+++ js/ica10.js
@@ -0,0 +1,6 @@
+document.getElementById("my_head").addEventListener("mouseenter", function(jerry){
+    document.getElementById("paragraph").innerHTML = "I, the adequate Morty! have changed the text!";
+    document.getElementById("paragraph").style.fontSize = "20px"
+    document.getElementById("my_body").style.backgroundColor = "red";
+    document.getElementById("image").src = "../img/Chris_main.jpg";
+})
